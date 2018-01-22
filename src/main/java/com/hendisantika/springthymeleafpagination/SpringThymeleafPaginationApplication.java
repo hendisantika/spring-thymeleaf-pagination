520fc6a6837dff5404bdfc37c39136d1e7f99ca5
Hendi Santika <hendisantika@gmail.com>
2018-01-23 06:21:12 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springthymeleafpagination/SpringThymeleafPaginationApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springthymeleafpagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafPaginationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringThymeleafPaginationApplication.class, args);
+	}
+}
