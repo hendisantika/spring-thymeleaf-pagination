ff3c2468adbed1700a9726bddf857f3bd02417a4
Hendi Santika <hendisantika@gmail.com>
2018-01-23 06:29:44 +0700

    #8 - Add Person Controller

---
+++ src/main/java/com/hendisantika/springthymeleafpagination/controller/PersonController.java
@@ -0,0 +1,69 @@
+package com.hendisantika.springthymeleafpagination.controller;
+
+import com.hendisantika.springthymeleafpagination.domain.Pager;
+import com.hendisantika.springthymeleafpagination.domain.Person;
+import com.hendisantika.springthymeleafpagination.service.PersonService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-thymeleaf-pagination
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 23/01/18
+ * Time: 06.29
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class PersonController {
+
+    private static final int BUTTONS_TO_SHOW = 5;
+    private static final int INITIAL_PAGE = 0;
+    private static final int INITIAL_PAGE_SIZE = 5;
+    private static final int[] PAGE_SIZES = {5, 10, 20};
+
+    private final PersonService personService;
+
+    public PersonController(PersonService studentService) {
+        this.personService = studentService;
+    }
+
+    /**
+     * Handles all requests
+     *
+     * @param pageSize
+     * @param page
+     * @return model and view
+     */
+    @GetMapping("/")
+    public ModelAndView showPersonsPage(@RequestParam("pageSize") Optional<Integer> pageSize,
+                                        @RequestParam("page") Optional<Integer> page) {
+        ModelAndView modelAndView = new ModelAndView("persons");
+
+        // Evaluate page size. If requested parameter is null, return initial
+        // page size
+        int evalPageSize = pageSize.orElse(INITIAL_PAGE_SIZE);
+        // Evaluate page. If requested parameter is null or less than 0 (to
+        // prevent exception), return initial size. Otherwise, return value of
+        // param. decreased by 1.
+        int evalPage = (page.orElse(0) < 1) ? INITIAL_PAGE : page.get() - 1;
+
+        Page<Person> persons = personService.findAllPageable(new PageRequest(evalPage, evalPageSize));
+        Pager pager = new Pager(persons.getTotalPages(), persons.getNumber(), BUTTONS_TO_SHOW);
+
+        modelAndView.addObject("persons", persons);
+        modelAndView.addObject("selectedPageSize", evalPageSize);
+        modelAndView.addObject("pageSizes", PAGE_SIZES);
+        modelAndView.addObject("pager", pager);
+        return modelAndView;
+    }
+
+}
